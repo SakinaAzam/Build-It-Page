57ed0b6b942575a777378e88fa7d22a1eb08eaf2
sakina <205117adp@gmail.com>
2024-07-01 22:16:21 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.fa-bars').addEventListener('click', () => {
+    document.querySelector('nav').style.right = '0px';
+})
+document.querySelector(".fa-xmark").addEventListener("click", () => {
+  document.querySelector("nav").style.right = "-300px";
+});
